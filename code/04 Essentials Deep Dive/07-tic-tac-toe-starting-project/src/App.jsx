933c8bcbc3cfab6cecb0b561e910f132c0b74a9c
jianing <ning94mw@gmail.com>
2024-07-14 18:34:39 +0800

    Tic Tac Toe completed

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Player from './Components/Player.jsx'
 import GameBoard from './Components/GameBoard.jsx'
 import Log from './Components/Log.jsx'
 import { WINNING_COMBINATIONS } from './Components/winning-combination.js'
+import GameOver from './Components/GameOver.jsx'
+
+const PLAYER = {
+  X: 'Player 1',
+  Y: 'Player 2',
+};
 
 const initailGameBoard = [
 
@@ -22,14 +28,8 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [gameTurns, setGameTurns] = React.useState([]); // [ {player: 'X', rowIndex: 0, colIndex: 1}, {player: 'Y', rowIndex: 1, colIndex: 2}
-  // const [activePlayer, setActivePlayer] = React.useState('X');
-  // const [hasWinner, setHasWinner] = React.useState(false);
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = initailGameBoard;
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...initailGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -37,14 +37,33 @@ function App() {
 
     gameBoard[row][col] = player;
   }
+  return gameBoard;
+}
 
-
+function deriveWinner(gameBoard, players) {
+  let winner;
 
   for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol
-    const secondSquareSymbol
-    const thirdSquareSymbol
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = players[firstSquareSymbol];
+    }
   }
+  return winner;
+}
+
+function App() {
+  const [player, setPlayer] = useState(PLAYER);
+  const [gameTurns, setGameTurns] = React.useState([]); // [ {player: 'X', rowIndex: 0, colIndex: 1}, {player: 'Y', rowIndex: 1, colIndex: 2}
+  // const [activePlayer, setActivePlayer] = React.useState('X');
+  // const [hasWinner, setHasWinner] = React.useState(false);
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, player);
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'Y' : 'X');
@@ -64,15 +83,29 @@ function App() {
     });
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayer((prevPlayer) => {
+      return {
+        ...prevPlayer,
+        [symbol]: newName,
+      };
+    });
+  }
+
   return <main>
     <div id='game-container'>
       <ol id='players' className='highlight-player'>
-        <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
-        <Player initialName='Player 2' symbol='Y' isActive={activePlayer === 'Y'}/>
+        <Player initialName={PLAYER.X} symbol='X' isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange}/>
+        <Player initialName={PLAYER.Y} symbol='Y' isActive={activePlayer === 'Y'} onChangeName={handlePlayerNameChange}/>
       </ol>
+      {(winner || hasDraw) && (<GameOver winner={winner} onRestart={handleRestart}/>)}
       <GameBoard
       onSelectSquare={handleSelectSquare}
-      board={gameBroad}
+      board={gameBoard}
       />
     </div>
     <Log turns={gameTurns} />
--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/Components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/Components/Player.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 
-export default function Player({initialName, symbol, isActive}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = React.useState(false);
 
@@ -9,8 +9,12 @@ export default function Player({initialName, symbol, isActive}) {
     // setIsEditing(isEditing ? false : true);
     // setIsEditing(!isEditing); // schedules a state update to the opposite of the current value
     setIsEditing(editing => !editing); // schedules a state update to true
-  }
 
+    if (isEditing) {
+    onChangeName(symbol, playerName);
+    }
+  }
+  
   function handleChange(event) {
     // console.log(event)
     setPlayerName(event.target.value);

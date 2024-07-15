b5b386bc6f16b055d841b8744bba868d1a48cecf
jianing <ning94mw@gmail.com>
2024-07-15 17:54:27 +0800

    result components

--- code/05 Essentials Practice/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,11 +1,31 @@
+import React, { useState } from 'react'
+
 import Header from './Components/Header.jsx'
 import UserInput from './Components/UserInput.jsx'
+import Results from './Components/Results.jsx'
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(preUserInput => {
+      return {
+        ...preUserInput,
+        [inputIdentifier]: +newValue
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onchange={handleChange}/>
+      <Results input={userInput} />
     </>
   );
 }

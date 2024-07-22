609212355272e3a302c249035250e3b943f2d7fd
jianing <ning94mw@gmail.com>
2024-07-22 17:36:42 +0800

    add timer

--- code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -1,27 +1,35 @@
 import React, { useState } from 'react';
 
 export default function TimerChallenge({ title, targetTime }) {
-  const [timerexpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart () {
     setTimeOut(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    
   }
 
   return (
   <section className='challenge'>
     <h2>{title}</h2>
+    {timerExpired && <p>You Lost!</p>}
     <p className='challenge-time'>
       {targetTime} seconds {targetTime === 1 ? 's' : ''} allowed to complete this challenge.
     </p>
     <p>
       <button>
-        Start Challenge
+        {timerStarted ? 'Stop' : 'Start'} Challenge
       </button>
     </p>
-    <p className=''>
-      Time is running.../ Timer inactive
+    <p className={timerStarted ? 'active' : undefined}>
+      {timerStarted ? "Time is running" : "Timer inactive" }
     </p>
   </section>
   );

3eadf5ec1150f1ff9ded86a86c783d87801079dd
jianing <ning94mw@gmail.com>
2024-07-22 19:17:28 +0800

    use showmodal and ref to get the pop message

--- code/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,13 +1,26 @@
-export default function ResultModal({ result, targetTime }) {
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+
+const ResultModel = forwardRef (function ResultModal({ result, targetTime }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    }
+  });
+
   return (
-  <dialog>
+  <dialog ref={dialog} className='result-modal'>
     <h2>You {result}</h2>
-    <p>The Target time was <strong>{targeTime} seconds.</strong></p>
+    <p>The Target time was <strong>{targetTime} seconds.</strong></p>
     <p>You stopped the timer with <strong>X seconds left</strong></p>
     <form method="dialog">
       <button>Close</button>
     </form>
   </dialog>
   );
+})
 
-}
+export default ResultModel;
--- code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -1,7 +1,9 @@
 import { useState, useRef } from 'react';
+import ResultModal from './ResultModal';
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -11,6 +13,7 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart () {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.open();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -23,10 +26,10 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-    {timerExpired && <ResultModal targetTime={targetTime} result='lost'/>}
+      <ResultModal ref={dialog} targetTime={targetTime} result='lost'/>
       <section className='challenge'>
         <h2>{title}</h2>
-        {timerExpired && <p>You Lost!</p>}
+
         <p className='challenge-time'>
           {targetTime} seconds {targetTime === 1 ? 's' : ''}
         </p>

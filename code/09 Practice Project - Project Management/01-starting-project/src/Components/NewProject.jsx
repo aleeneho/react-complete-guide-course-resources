bbe68d9d6aa35841dcd8d34e55ed2b88a32498ce
jianing <ning94mw@gmail.com>
2024-07-23 18:23:05 +0800

    modal components added for dialog pop out

--- code/09 Practice Project - Project Management/01-starting-project/src/Components/NewProject.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/Components/NewProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from 'react';
 import Input from './Input';
+import Modal from './Modal';
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -10,6 +12,15 @@ export default function NewProject({ onAdd }) {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
+
+    if (enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredDueDate.trim() === ''
+      ) {
+        modal.current.open();
+        return;
+
+    }
 
     onAdd({
       title: enteredTitle,
@@ -20,15 +31,22 @@ export default function NewProject({ onAdd }) {
 
 
   return (
-   <div className='w-[35rem] mt-16'>
-    <menu className='flex items-center justify-end gap-4 my-4'>
-      <li>
-        <button className='text-stone-800 hover:text=stone-950 '>Cancel</button>
-        </li>
-      <li>
-        <button onClick={handleSave} className='px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button>
-        </li>
-    </menu>
+    <>
+    <Modal ref={modal} buttonCaption="Okay">
+      <h2>Invalid Input</h2>
+      <p>Oops.. looks like you forgot to enter a value.</p>
+      <p>Please amke sure you provide a valid value for every input field.</p>
+    </Modal>
+
+     <div className='w-[35rem] mt-16'>
+      <menu className='flex items-center justify-end gap-4 my-4'>
+        <li>
+          <button className='text-stone-800 hover:text=stone-950 '>Cancel</button>
+          </li>
+        <li>
+          <button onClick={handleSave} className='px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button>
+          </li>
+      </menu>
 
     <div>
       <Input type='text' ref={title} label='Title' />
@@ -36,6 +54,7 @@ export default function NewProject({ onAdd }) {
       <Input type='date' ref={dueDate}label='Due Date' />
     </div>
 
-   </div>
+     </div>
+    </>
   )
 }
